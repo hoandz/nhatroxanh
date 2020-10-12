d24261b76cb03a2b1617f02f3b94448e4d04f555
Hoan Tran <hoantran.us@gmail.com>
2020-10-12 17:34:57 +0700

    asdf

--- components/propertyListing.js
+++ components/propertyListing.js
@@ -48,8 +48,8 @@ var propertyListing = Vue.component('propertyListing', {
               <div class="price">2 triệu</div>
             </li>
             <li class="property-item">
-              <img class="lozad" alt="" src="https://cloud.mogi.vn/images/thumb-xs/2020/06/11/339/8915f665192843efa9a7341c4816721e.jpg" data-loaded="true" />
-              <a href="/article" class="title link-overlay" gtm-event="link" gtm-cat="home" gtm-act="property-latest">Bán nhà dãy nhà trọ khu tái định cư êm đềm,gần kcx linh trung1 th đức.</a>
+              <img class="lozad" alt="" src="https://cloud.mogi.vn/images/thumb-small/2020/08/13/161/9d92e59686d147a3b3d68a528386594f.jpg" data-loaded="true" />
+              <a href="./article.html" class="title link-overlay" gtm-event="link" gtm-cat="home" gtm-act="property-latest">Bán nhà dãy nhà trọ khu tái định cư êm đềm,gần kcx linh trung1 th đức.</a>
               <ul class="prop-attr">
                 <li><i class="fa fa-chart-area"></i>121 m<sup>2</sup></li>
                 <li><i class="fa fa-bed"></i>8</li>
@@ -58,7 +58,7 @@ var propertyListing = Vue.component('propertyListing', {
               <div class="price">2.5 triệu</div>
             </li>
             <li class="property-item">
-              <img class="lozad" alt="" src="https://cloud.mogi.vn/images/thumb-xs/2020/06/11/339/8915f665192843efa9a7341c4816721e.jpg" data-loaded="true" />
+              <img class="lozad" alt="" src="https://cloud.mogi.vn/images/thumb-small/2020/07/06/349/109894f4096f4539ab1b017e867db7d6.jpg" data-loaded="true" />
               <a href="./article.html" class="title link-overlay" gtm-event="link" gtm-cat="home" gtm-act="property-latest">Cho thuê dãy nhà trọ khu tái định cư êm đềm,gần kcx linh trung1 th đức.</a>
               <ul class="prop-attr">
                 <li><i class="fa fa-chart-area"></i>121 m<sup>2</sup></li>
@@ -68,17 +68,17 @@ var propertyListing = Vue.component('propertyListing', {
               <div class="price">5 triệu</div>
             </li>
             <li class="property-item">
-              <img class="lozad" alt="" src="https://cloud.mogi.vn/images/thumb-xs/2020/06/11/339/8915f665192843efa9a7341c4816721e.jpg" data-loaded="true" />
+              <img class="lozad" alt="" src="https://cloud.mogi.vn/images/thumb-small/2020/08/28/492/e2c5e373d9e94823b28fba1f12538be3.jpg" data-loaded="true" />
               <a href="./article.html" class="title link-overlay" gtm-event="link" gtm-cat="home" gtm-act="property-latest">Cho thuê dãy nhà trọ khu tái định cư êm đềm,gần kcx linh trung1 th đức.</a>
               <ul class="prop-attr">
                 <li><i class="fa fa-chart-area"></i>121 m<sup>2</sup></li>
                 <li><i class="fa fa-bed"></i>8</li>
                 <li><i class="fa fa-bath"></i>8</li>
               </ul>
-              <div class="price">5 tỷ 200 triệu</div>
+              <div class="price">3 triệu</div>
             </li>
             <li class="property-item">
-              <img class="lozad" alt="" src="https://cloud.mogi.vn/images/thumb-xs/2020/06/11/339/8915f665192843efa9a7341c4816721e.jpg" data-loaded="true" />
+              <img class="lozad" alt="" src="https://cloud.mogi.vn/images/thumb-small/2020/06/28/384/28478a24b0c447b88ae2982d2e47311b.jpg" data-loaded="true" />
               <a href="./article.html" class="title link-overlay" gtm-event="link" gtm-cat="home" gtm-act="property-latest">Cho thuê dãy nhà trọ khu tái định cư êm đềm,gần kcx linh trung1 th đức.</a>
               <ul class="prop-attr">
                 <li><i class="fa fa-chart-area"></i>121 m<sup>2</sup></li>
@@ -88,14 +88,14 @@ var propertyListing = Vue.component('propertyListing', {
               <div class="price">5 triệu</div>
             </li>
             <li class="property-item">
-              <img class="lozad" alt="" src="https://cloud.mogi.vn/images/thumb-xs/2020/06/11/339/8915f665192843efa9a7341c4816721e.jpg" data-loaded="true" />
+              <img class="lozad" alt="" src="https://cloud.mogi.vn/images/thumb-xs/2020/10/05/529/8c29a982845249eb8ff43269909ea308.jpg" data-loaded="true" />
               <a href="./article.html" class="title link-overlay" gtm-event="link" gtm-cat="home" gtm-act="property-latest">Cho thuê dãy nhà trọ khu tái định cư êm đềm,gần kcx linh trung1 th đức.</a>
               <ul class="prop-attr">
                 <li><i class="fa fa-chart-area"></i>121 m<sup>2</sup></li>
                 <li><i class="fa fa-bed"></i>8</li>
                 <li><i class="fa fa-bath"></i>8</li>
               </ul>
-              <div class="price">5 tỷ 200 triệu</div>
+              <div class="price">6 triệu</div>
             </li>
           </ul>
         </div>
--- components/search.js
+++ components/search.js
@@ -1,33 +1,66 @@
+// <div class="search">
+//           <div class="input-group mb-3">
+//             <div class="input-group-prepend">
+//               <span class="input-group-text color-search" id="basic-addon1"><i class="fas fa-search"></i></span>
+//             </div>
+//             <input type="text" class="form-control bg-light color-search" placeholder="Từ khóa, Đường, Quận, Dự án hoặc địa danh ..." aria-label="Username" aria-describedby="basic-addon1" />
+//             <div class="input-group-prepend">
+//               <span class="input-group-text color-search btn-search" id="basic-addon1">Tìm kiếm</span>
+//             </div>
+//           </div>
+//         </div>
+//         <div class="services">
+//         </div>
+
 var search = Vue.component('search', {
   template: `
     <div class="search-bar-box">
       <h2>Đúng nhà, đúng giá, đúng thời điểm</h2>
       <div class="search-bar">
-        <div class="services">
-        </div>
-        <div class="search">
-          <div class="input-group mb-3">
-            <div class="input-group-prepend">
-              <span class="input-group-text color-search" id="basic-addon1"><i class="fas fa-search"></i></span>
-            </div>
-            <input type="text" class="form-control bg-light color-search" placeholder="Từ khóa, Đường, Quận, Dự án hoặc địa danh ..." aria-label="Username" aria-describedby="basic-addon1" />
-            <div class="input-group-prepend">
-              <span class="input-group-text color-search btn-search" id="basic-addon1">Tìm kiếm</span>
-            </div>
+        <div class="container-search">
+          <div class="search">
+           <div class="input-group mb-3">
+             <div class="input-group-prepend">
+               <span class="input-group-text color-search" id="basic-addon1"><i class="fas fa-search"></i></span>
+             </div>
+             <input type="text" class="form-control bg-light color-search" placeholder="Từ khóa, Đường, Quận, Dự án hoặc địa danh ..." aria-label="Username" aria-describedby="basic-addon1" />
+             <div class="input-group-prepend">
+               <span class="input-group-text color-search btn-search" id="basic-addon1">Tìm kiếm</span>
+             </div>
+           </div>
           </div>
-        </div>
-        <div class="search-filter">
-          <div class="row">
-            <select class="col-xs-4 col-sm-4 col-md-4 form-control" name="calc_shipping_provinces" required="">
-              <option value="">Tỉnh / Thành phố</option>
-            </select>
-            <select class="col-xs-4 col-sm-4 col-md-4 form-control" name="calc_shipping_district" required="">
-              <option value="">Quận / Huyện</option>
-            </select>
+          <div class="search-filter">
+            <div class="select-1">
+              <select class="form-control" name="calc_shipping_provinces" required="">
+                <option value="">Tỉnh / Thành phố</option>
+              </select>
+            </div>
+            <div class="select-2">
+              <select class="form-control" name="calc_shipping_district" required="">
+                <option value="">Quận / Huyện</option>
+              </select>
+            </div>
+            <div class="select-3">
+              <select class="form-control" required="">
+                <option value="">Giá từ</option>
+                <option value="">1.000.000</option>
+                <option value="">2.000.000</option>
+                <option value="">3.000.000</option>
+              </select>
+            </div>
+            <div class="select-4">
+              <select class="form-control" required="">
+                <option value="">Giá max</option>
+                <option value="">3.000.000</option>
+                <option value="">4.000.000</option>
+                <option value="">5.000.000</option>
+                <option value="">6.000.000</option>
+              </select>
+            </div>
             <input class="billing_address_1" name="" type="hidden" value="">
-            <input class="billing_address_2" name="" type="hidden" value="">  
-          </div>        
-        </div>
+            <input class="billing_address_2" name="" type="hidden" value="">      
+          </div>
+        </div> 
       </div>
     </div>
   `,
